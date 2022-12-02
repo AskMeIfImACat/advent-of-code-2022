c3db05bd4c29c98091ae289cd0f39ae59c8d07e9
Antoine Morin <antoine.morin@ariv.ca>
2022-12-01 20:15:39 -0500

    Add the missing test declaration and assertion

--- Day 1/CalorieCounting.Tests/ProgramTests.cs
+++ Day 1/CalorieCounting.Tests/ProgramTests.cs
@@ -2,9 +2,11 @@
 
 public class ProgramTests
 {
+    [Fact]
     public void CanGetTheHighestCaloriesTotalFromAllInventories()
     {
         var highestCaloriesTotal = new Program().GetHighestCaloriesTotal("test-inventory.data");
 
+        Assert.Equal(24000, highestCaloriesTotal);
     }
 }
