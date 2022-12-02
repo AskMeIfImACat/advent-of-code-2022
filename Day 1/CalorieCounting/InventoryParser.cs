3b32156d14e9d3b79695fc5f5aad715e5cf916a6
Antoine Morin <antoine.morin@ariv.ca>
2022-12-01 20:55:23 -0500

    Apply some minor code formatting fixes

--- Day 1/CalorieCounting/InventoryParser.cs
+++ Day 1/CalorieCounting/InventoryParser.cs
@@ -1,44 +1,43 @@
-﻿namespace CalorieCounting
+﻿namespace CalorieCounting;
+
+public class InventoryParser
 {
-    public class InventoryParser
+    private readonly string filePath;
+
+    public InventoryParser(string filePath)
     {
-        private string filePath;
-
-        public InventoryParser(string filePath)
-        {
-            this.filePath = filePath;
-        }
-
-        public IEnumerable<Inventory> GetInventories()
-        {
-            var inventoryLines = File.ReadLines(filePath).GetEnumerator();
-
-            while (inventoryLines.MoveNext())
-            {
-                yield return GetSingleInventory(inventoryLines);
-            }
-        }
-
-        private Inventory GetSingleInventory(IEnumerator<string> inventoryLines)
-        {
-            var inventory = new Inventory();
-
-            do
-            {
-                if (IsEndOfInventory(inventoryLines.Current))
-                    break;
-
-                inventory.Calories.Add(ExtractCalories(inventoryLines.Current));
-
-            } while (inventoryLines.MoveNext());
-
-            return inventory;
-        }
-
-        private static int ExtractCalories(string inventoryLine)
-            => int.Parse(inventoryLine);
-
-        private static bool IsEndOfInventory(string inventoryFileLine)
-            => string.IsNullOrWhiteSpace(inventoryFileLine);
+        this.filePath = filePath;
     }
+
+    public IEnumerable<Inventory> GetInventories()
+    {
+        var inventoryLines = File.ReadLines(filePath).GetEnumerator();
+
+        while (inventoryLines.MoveNext())
+        {
+            yield return GetSingleInventory(inventoryLines);
+        }
+    }
+
+    private static Inventory GetSingleInventory(IEnumerator<string> inventoryLines)
+    {
+        var inventory = new Inventory();
+
+        do
+        {
+            if (IsEndOfInventory(inventoryLines.Current))
+                break;
+
+            inventory.Calories.Add(ParseCalories(inventoryLines.Current));
+
+        } while (inventoryLines.MoveNext());
+
+        return inventory;
+    }
+
+    private static int ParseCalories(string inventoryLine)
+        => int.Parse(inventoryLine);
+
+    private static bool IsEndOfInventory(string inventoryFileLine)
+        => string.IsNullOrWhiteSpace(inventoryFileLine);
 }
